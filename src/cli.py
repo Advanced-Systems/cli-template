157f8676ba15c8d3616e74cacd4bddb75223b9a9
StefanGreve <greve.stefan@outlook.jp>
2021-01-17 19:37:34 +0100

    Add template code with intact imports

---
+++ src/cli.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from src import utils
+
+import click
+import pretty_errors
+
+from .__init__ import __version__
+from .core import square_function
+
+@click.group(invoke_without_command=True)
+@click.version_option(version=__version__, prog_name='cli-template')
+@click.pass_context
+def cli(ctx):
+    ctx.ensure_object(dict)
+    ctx.obj['CONFIG'] = utils.read_configuration('src.data', 'config.json')
+
+@cli.command(help="Prints hello world.")
+@click.pass_context
+def test(ctx):
+    config = ctx.obj['CONFIG']
+
+    # imported from config
+    click.secho(config.get('Message', 'KeyNotFoundError'), fg='yellow')
+    
+    # imported from core
+    click.echo("First ten powers of two:")
+    for x, y in enumerate(square_function(1, 10)):
+        click.echo(f"x={x+1}\ty={int(y)}")
