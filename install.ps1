32c458d5b642cc1ddfe464bc09b562cde8892438
StefanGreve <greve.stefan@outlook.jp>
2021-01-22 20:12:47 +0100

    Fix typos in script

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$PackageName = Read-Host "Enter package name"
+$PackageName = Read-Host "Enter your package name"
 Write-Host "Creating a new virtual environment . . ." -ForegroundColor Yellow
 py.exe -3.8 -m venv venv/
 venv/Scripts/Activate.ps1
@@ -6,5 +6,5 @@ python -m pip install --upgrade pip
 Write-Host "Installing $PackageName . . ." -ForegroundColor Yellow 
 pip install -e .
 Write-Host "Installing additional dev dependencies . . ." -ForegroundColor Yellow
-pip install -r .\requirements/dev.txt
+pip install -r requirements/dev.txt
 Write-Host "Installation is complete!" -ForegroundColor Green
