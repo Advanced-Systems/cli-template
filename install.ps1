a11d0ab34f6535d0ea30db067137c1480111b93f
StefanGreve <greve.stefan@outlook.jp>
2021-01-22 20:16:49 +0100

    Remove prompt that no longer serves any purpose

--- install.ps1
+++ install.ps1
@@ -1,9 +1,8 @@
-$PackageName = Read-Host "Enter your package name"
 Write-Host "Creating a new virtual environment . . ." -ForegroundColor Yellow
 py.exe -3.8 -m venv venv/
 venv/Scripts/Activate.ps1
 python -m pip install --upgrade pip
-Write-Host "Installing $PackageName . . ." -ForegroundColor Yellow 
+Write-Host "Installing package . . ." -ForegroundColor Yellow 
 pip install -e .
 Write-Host "Installing additional dev dependencies . . ." -ForegroundColor Yellow
 pip install -r requirements/dev.txt
