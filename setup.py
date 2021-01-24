fd3b27d17885edc44951c2b5713a845ddf2eaa31
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-01-24 22:43:57 +0100

    Improve setup.py file

--- setup.py
+++ setup.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
 
-import re
+import sys
 
 from setuptools import setup
 
-with open("src/__init__.py", encoding='utf8') as file_handler:
-    lines = file_handler.read()
-    version = re.search(r'__version__ = "(.*?)"', lines).group(1)
-    package_name = re.search(r'package_name = "(.*?)"', lines).group(1)
+from src.__init__ import __version__, package_name, python_major, python_minor
+
+if package_name == 'cli-template':
+    print("\033[93mWARNING: You should rename the default package name.\033[0m")
+
+try:
+    assert (sys.version_info.major == python_major and sys.version_info.minor <= python_minor)
+except AssertionError:
+    raise RuntimeError("\033[91mPython Version %d.%d+ is required!\033[0m" % (python_major, python_minor))
+
+print("reading dependency file")
 
 with open("requirements/release.txt", encoding='utf-8') as file_handler:
     packages = file_handler.read().splitlines()
 
-# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+print("intitializing %s's setup routine" % package_name)
+
 setup(
     name=package_name,
-    version=version,
+    version=__version__,
+    python_requires=">=%d.%d" % (python_major, python_minor),
     install_requires=packages,
     include_package_data=True,
-    entry_points=f'''
+    entry_points='''
         [console_scripts]
-        {package_name}=src.__main__:cli
-    ''',
+        %s=src.__main__:cli
+    ''' % package_name,
 )
+
+wheel_name = package_name.replace('-', '_') if '-' in package_name else package_name
+print("\033[92mSetup is complete. Run 'python -m pip install dist/%s-%s-py%d-none-any.whl' to install this wheel.\033[0m" % (wheel_name, __version__, python_major))
