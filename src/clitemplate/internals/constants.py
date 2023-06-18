a2b11c1ea2c4cbcb2a8a72bdc76b6913a419ce7c
StefanGreve <greve.stefan@outlook.jp>
2023-06-18 16:40:36 +0200

    Bind enum to package name

--- src/clitemplate/internals/constants.py
+++ src/clitemplate/internals/constants.py
@@ -2,7 +2,9 @@
 
 from enum import Enum, unique
 
+from ..__init__ import package_name
+
 @unique
 class Files(Enum):
-    LOG_FILE = "cli-template.log"
-    CONFIG_FILE = "cli-template.ini"
+    LOG_FILE = f"{package_name}.log"
+    CONFIG_FILE = f"{package_name}.ini"
